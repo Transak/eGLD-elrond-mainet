a463c11ec5bce1370710d6293238a865202c5d15
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2022-05-27 07:43:09 +0530

    fix(engcc-316): add support for esdt token transfers. -functions modified getTransaction now has decimals paramater added for esdt tokens. - sendTransaction now has tokenIdentifier, contractAddress, decimals fields added to suppport esdt which are recognised by tokenIdentifier and decimals is required for formating balances and creating payment, - getBalance function accepts extra object as param with tokenIdentifier and decimals as properties.

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 module.exports = {
     networks : {
         main: {
-            provider: 'https://api.elrond.com',
+            provider: 'https://gateway.elrond.com',
             transactionLink : (hash) => `https://explorer.elrond.com/transactions/${hash}`,
             walletLink : (address) => `https://explorer.elrond.com/address/${address}`,
             networkName: 'main',
 
         },
         testnet: {
-            provider: 'https://testnet-api.elrond.com',
+            provider: 'https://testnet-gateway.elrond.com',
             transactionLink : (hash) => `https://testnet-explorer.elrond.com/transactions/${hash}`,
             walletLink : (address) => `https://testnet-explorer.elrond.com/address/${address}`,
             networkName: 'testnet',
--- test/elrond.test.js
+++ test/elrond.test.js
@@ -1,6 +1,6 @@
-const elrondLib = require('../index');
-const {expect, assert} = require('chai');
-require("dotenv").config({path: `${__dirname}/.env`})
+const elrondLib = require("../index");
+const { expect, assert } = require("chai");
+require("dotenv").config({ path: `${__dirname}/.env` });
 /*
 invalid chain id
  */
@@ -11,29 +11,17 @@ const testData = {
     network: process.env.NETWORK,
     keyStore: process.env.KEYSTORE,
     password: process.env.PASSWORD,
-    amount: 0.00005
+    amount: 0.00005,
+    identifier: "ZPAY-eb1ced",
+    decimals_ESDT: 18,
 };
-if (!testData.keyStore || !testData.password) throw new Error('Invalid keyStore or password')
+if (!testData.keyStore || !testData.password) throw new Error("Invalid keyStore or password");
 
 const runtime = {};
 
-
 const keys = {
-    sendTransaction: [
-        "amount",
-        "date",
-        "from",
-        "gasCostCryptoCurrency",
-        "gasCostInCrypto",
-        "gasLimit",
-        "gasPrice",
-        "network",
-        "nonce",
-        "to",
-        "transactionHash",
-        "transactionLink"
-    ],
-    getTransaction : [
+    sendTransaction: ["amount", "date", "from", "gasCostCryptoCurrency", "gasCostInCrypto", "gasLimit", "gasPrice", "network", "nonce", "to", "transactionHash", "transactionLink"],
+    getTransaction: [
         "amount",
         "date",
         "from",
@@ -49,9 +37,10 @@ const keys = {
         "network",
         "nonce",
         "to",
+        "tokenIdentifier",
         "transactionHash",
-        "transactionLink"
-    ]
+        "transactionLink",
+    ],
 };
 
 // validate object by all it's keys
@@ -62,45 +51,78 @@ const allKeys = (result, keys) => {
 };
 
 describe("eGLD-elrond-mainet module", () => {
-
-    it("should getBalance", async function () {
+    it("should getBalance egld", async function() {
         this.timeout(mainTimeout * 3);
         const result = await elrondLib.getBalance(testData.toWalletAddress, testData.network);
+        console.log({ balance: result });
         expect(typeof result === "number");
     });
 
-    it("should isValidWalletAddress", async function () {
+    it("should getBalance esdt", async function() {
+        this.timeout(mainTimeout * 3);
+        const result = await elrondLib.getBalance(testData.toWalletAddress, testData.network, {tokenIdentifier: testData.identifier, decimals:testData.decimals_ESDT});
+        console.log({ balance: result });
+        expect(typeof result === "number");
+    });
+
+    it("should isValidWalletAddress", async function() {
         this.timeout(mainTimeout * 3);
         const result = await elrondLib.isValidWalletAddress(testData.toWalletAddress, testData.network);
         expect(result === true);
     });
 
-    it("should sendTransaction", async function () {
+    it("should sendTransaction", async function() {
         this.timeout(mainTimeout * 3);
-        const {
-            toWalletAddress: to,
-            keyStore,
-            password,
-            network,
-            amount
-        } = testData;
+        const { toWalletAddress: to, keyStore, password, network, amount } = testData;
 
         const result = await elrondLib.sendTransaction({
             to,
             amount,
             network,
-            keyStore, password
+            keyStore,
+            password,
         });
+        console.log(result);
         assert.hasAllKeys(result.receipt, keys.sendTransaction);
         runtime.transactionHash = result.receipt.transactionHash;
     });
 
-    it("should getTransaction", async function () {
+    it("should getTransaction", async function() {
         this.timeout(mainTimeout * 3);
-        const {
-            network,
-        } = testData;
+        console.log("NORMAl \n \n");
+        const { network } = testData;
         const result = await elrondLib.getTransaction(runtime.transactionHash, network);
+        assert.hasAllKeys(result.receipt, keys.getTransaction);
+    });
+
+    it("should sendTransaction ESDT", async function() {
+        this.timeout(mainTimeout * 3);
+        const { toWalletAddress: to, keyStore, password, network, amount, identifier, decimals_ESDT } = testData;
+
+        const result = await elrondLib.sendTransaction({
+            to,
+            amount,
+            network,
+            keyStore,
+            password,
+            tokenIdentifier: identifier,
+            decimals: decimals_ESDT,
+        });
+        console.log(result);
+        assert.hasAllKeys(result.receipt, keys.sendTransaction);
+        runtime.transactionHashEsdt = result.receipt.transactionHash;
+    });
+
+    it("should getTransaction ESDT", async function() {
+        this.timeout(mainTimeout * 3);
+        console.log("ESDT \n\n");
+        const { network } = testData;
+        const result = await elrondLib.getTransaction(runtime.transactionHashEsdt, network);
+        console.log(result);
+        // console.log("contractResults")
+        // console.log(result.transactionData.contractResults)
+        // console.log("logs")
+        // console.log(result.transactionData.logs.events[0])
         assert.hasAllKeys(result.receipt, keys.getTransaction);
     });
 });
