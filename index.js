a97834a9f4b7997caa4dc22ed5c1602dc6f1ef4b
Minhaj-ul-abidin <minhaj3labidin@gmail.com>
2022-05-31 00:17:32 +0530

    added consleError function with custom tags , pinned versions in package json to exact match, moved utility functions to utils , added joi validation.

--- index.js
+++ index.js
@@ -1,63 +1,58 @@
 const config = require('./config');
-const BigNumber = require('bignumber.js');
-const { TransactionPayload, ESDTTransferPayloadBuilder, } = require("@elrondnetwork/erdjs");
-const { TokenPayment, Address, Transaction, Account, TransactionHash, USER } = require("@elrondnetwork/erdjs");
+const { Address, Transaction, Account, TransactionHash, TransactionWatcher } = require("@elrondnetwork/erdjs");
 const { ProxyNetworkProvider } = require("@elrondnetwork/erdjs-network-providers");
-const { TransactionDecoder, TransactionMetadata} = require("@elrondnetwork/transaction-decoder");
-const { UserSigner } = require("@elrondnetwork/erdjs-walletcore");
 const timeout = 20000;
 const _ = require('lodash');
-const { assert } = require('chai');
-const _toDecimal = (amount, decimals) => {
-    return new BigNumber(amount).div(`1e${decimals}`).toString(10);
-}
-
-/**
- * 
- * @param {string} keyStore     
- * @param {string} password 
- * @returns {UserSigner} signer object
- */
-const _createSigner = (keyStore, password) => {
-    if (!keyStore) throw new Error('keyStore is undefined')
-    if (!password) throw new Error('password is undefined')
-    try {
-        const keyFileObject = JSON.parse(keyStore);
-        const signer = UserSigner.fromWallet(keyFileObject, password);
-        return signer;
-    } catch (e) {
-        console.error(e)
-        throw new Error('Invalid keyStore or password')
-    }
-}
+const {consoleError, _toDecimal, _createSigner, _getTokenTransferred, _getPayload,sleep} = require("./utils");
+const {getTransactionLinkParams ,getWalletLinkParams, getBalanceParams, getTransactionParams, sendTransactionParams} = require("./validator");
 
 function getTransactionLink(txId, network) {
-    let networkDetails = config.networks.testnet;
+    const {error} = getTransactionLinkParams.validate({txId, network});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
+
+   let networkDetails = config.networks.testnet;
     if (network === 'main') networkDetails = config.networks.main;
     return networkDetails.transactionLink(txId);
 }
 
 function getWalletLink(walletAddress, network) {
+    const {error} = getWalletLinkParams.validate({walletAddress, network});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
+
     let networkDetails = config.networks.testnet;
     if (network === 'main') networkDetails = config.networks.main;
     return networkDetails.walletLink(walletAddress);
 }
 
+/**
+ * 
+ * @param {string} address 
+ * @param {string} network 
+ * @param {*} param2 - object
+ * - {string} tokenIdentifier - optional
+ * - {number} decimals - optional
+ * @returns 
+ */
 async function getBalance(address, network, {tokenIdentifier, decimals}={}) {
-    if(tokenIdentifier) assert.ok(_.isNumber(decimals), "decimals required if identifier provided in options")
+    const {error} = getBalanceParams.validate({address, network, options:{tokenIdentifier, decimals}});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
     try {
         let networkDetails = config.networks.testnet;
         if (network === 'main') networkDetails = config.networks.main;
         const provider = new ProxyNetworkProvider(networkDetails.provider, {timeout});
         address = new Address(address);
         const account = await provider.getAccount(address);
-      
-
-        if (tokenIdentifier && decimals) { 
+        if (tokenIdentifier) { 
             try {
                 rawBalance  = (await provider.getFungibleTokenOfAccount(address, tokenIdentifier)).balance.toString()
             } catch (e) {
-                console.error(e)
+                consoleError({message: e.message,err: e,tags:{method: "getBalance"}})
                 rawBalance = "0"; 
             }
         }
@@ -68,18 +63,22 @@ async function getBalance(address, network, {tokenIdentifier, decimals}={}) {
          
         if (rawBalance) return Number(_toDecimal(rawBalance, decimals));
     } catch (e) {
-        console.error(e)
+        consoleError({message: e.message, err: e, tags:{method: "getBalance"}})
         return false;
     }
 }
 
 /**
  * 
- * @param {string} address 
+ * @param {string} walletAddress
  * @param {string} network 
- * @returns  {bool} true if address is valid, false otherwise
+ * @returns {bool} true if address is valid, false otherwise
  */
-async function isValidWalletAddress(address, network) {
+async function isValidWalletAddress(walletAddress, network) {
+    const {error} = getWalletLinkParams.validate({walletAddress, network});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
     try {
         let networkDetails = config.networks.testnet;
         if (network === 'main') networkDetails = config.networks.main;
@@ -92,28 +91,13 @@ async function isValidWalletAddress(address, network) {
     }
 }
 
-/**
- * 
- * @param {object} parameters {transactionData, decimals}
- * -  {object} transactionData - transaction data object
- * - {number} decimals - token decimals
- * @returns {object} {amount : {Number}, tokenIdentifier: {string}}
- */
-function getTokenTransferred({transactionData, decimals}) {
-    const decodedTransaction = new TransactionDecoder().getTransactionMetadata(transactionData);
-    let tranferedTokens, identifier;
-    if (decodedTransaction.transfers && decodedTransaction.transfers.length > 0) {
-        tranferedTokens = Number(_toDecimal(decodedTransaction.transfers[0].value.toString(), decimals));
-        identifier= _.get(decodedTransaction.transfers[0],"properties.identifier", "");
 
-    } else {
-        tranferedTokens = Number(_toDecimal(decodedTransaction.value.toString(), decimals));
-        identifier = "EGLD"
-    }
-    return { tranferedTokens, identifier };
-}
 
 async function getTransaction(hash, network, decimals=18) {
+    const {error} = getTransactionParams.validate({hash, network, decimals});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
     let response = false;
     try {
         if (hash) {
@@ -126,7 +110,7 @@ async function getTransaction(hash, network, decimals=18) {
             const txHashString = txnHash.toString()
             const transaction = await provider.getTransaction(txnHash);
             
-            const {tranferedTokens, identifier} =  getTokenTransferred({transactionData: transaction, decimals: decimals})
+            const {tranferedTokens, identifier} =  _getTokenTransferred({transactionData: transaction, decimals: decimals})
             transaction.value = Number(_toDecimal(transaction.value, 18))
             if (transaction) {
                 response = {
@@ -167,37 +151,7 @@ async function getTransaction(hash, network, decimals=18) {
         }
         return response
     } catch (e) {
-        console.error(e)
-        // throw(e.message)
-    }
-}
-
-
-/**
- * 
- * @param {*} param0 - {identifier, amount, decimals}
- * - {string| undefined} identifier - token identifier 
- * - {number} amount - token amount
- * - {number| undefined} decimals - token decimals
- * @returns {object} - {payment : {Payment|undefined} , payload: {TransactionPayload}, transaction: {Transaction}}
- */
-function _getPayload({identifier, amount, decimals}){
-    assert.ok(amount, 'amount is required')
-    if(identifier) assert.ok(_.isNumber(decimals), 'decimals is required')
-    let payment , payload, gasLimit
-    if (identifier) {
-        payload = new ESDTTransferPayloadBuilder().setPayment(TokenPayment.fungibleFromAmount(identifier,amount.toString(), decimals)).build()
-        gasLimit = 50000 + 1500 * payload.length() + 300000
-    } else {
-        payment = TokenPayment.egldFromAmount(amount.toString())
-        payload =  new TransactionPayload("")
-        gasLimit = 70000
-    }
-
-    return {
-        payment, 
-        payload, 
-        gasLimit
+        consoleError({message: e.message, err: e, tags: {method: "getTransaction"}})
     }
 }
 
@@ -209,19 +163,15 @@ function _getPayload({identifier, amount, decimals}){
  * - {string} network - network name (main or testnet)
  * - {string} keyStore - keystore file content
  * - {string} password - keystore password
- * - {string} contractAddress - address of the token contract
  * - {string} tokenIdentifier - token identifier
  * - {number} decimals - token decimals
- * 
  * @returns {object} - { transactionData , receipt}
  */
-async function sendTransaction({to, amount, network, keyStore, password, contractAddrress, tokenIdentifier, decimals}) {
-    assert.ok(to, 'to address is required');
-    assert.ok(amount, 'amount is required');
-    assert.ok(network, 'network is required');
-    assert.ok(keyStore, 'keyStore is required');
-    assert.ok(password, 'password is required');
-    if ((contractAddrress||tokenIdentifier)) assert.ok(_.isNumber(decimals), 'decimals is required if either contractAddress or identifier is supplied');
+async function sendTransaction({to, amount, network, keyStore, password, tokenIdentifier, decimals}) {
+    const {error} = sendTransactionParams.validate({to, amount, network, keyStore, password, options: {tokenIdentifier, decimals}});
+    if (error) {
+        throw new Error(`${error.message}`)
+    }
     try {
         //Set network
         let networkDetails = config.networks.testnet;
@@ -259,7 +209,7 @@ async function sendTransaction({to, amount, network, keyStore, password, contrac
         });
         signer.sign(transaction);
         const txHash = await provider.sendTransaction(transaction);
-        const {tranferedTokens} =  getTokenTransferred({transactionData: transaction, decimals: decimals})
+        const {tranferedTokens} =  _getTokenTransferred({transactionData: transaction, decimals: decimals})
         return {
             transactionData: transaction,
             receipt: {
@@ -278,10 +228,10 @@ async function sendTransaction({to, amount, network, keyStore, password, contrac
             }
         };
     } catch (e) {
+        consoleError({message:e.message,err: e,tags: {method: "sendTransaction"}})
         throw(e)
     }
 }
-
 
 module.exports = {
     getTransactionLink,
